ee121d82b46f25dd899765c63126b9bcef090634
Usman Tahir <Usman.tahir@synergy-it.com>
2023-08-27 22:12:00 +0500

    Migration run automatically code implementation

--- Src/Presentation/AuthApi/Program.cs
+++ Src/Presentation/AuthApi/Program.cs
@@ -4,33 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 namespace AuthApi {
-    //public class Program {
-    //    public static void Main(string[] args) {
-    //        var builder = WebApplication.CreateBuilder(args);
 
-    //        // Add services to the container.
-
-    //        // builder.Services.AddControllers();
-    //        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-    //        builder.Services.AddEndpointsApiExplorer();
-    //        builder.Services.AddSwaggerGen();
-
-    //        var app = builder.Build();
-
-    //        // Configure the HTTP request pipeline.
-    //        if (app.Environment.IsDevelopment()) {
-    //            app.UseSwagger();
-    //            app.UseSwaggerUI();
-    //        }
-
-    //        app.UseAuthorization();
-
-
-    //        app.MapControllers();
-
-    //        app.Run();
-    //    }
-    //}
 
     public class Program {
         public static async Task Main(string[] args) {
@@ -45,6 +19,7 @@ namespace AuthApi {
             var host = CreateHostBuilder(args).Build();
             using (var scope = host.Services.CreateScope()) {
                 var services = scope.ServiceProvider;
+                var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
                 try {
                     var context = services.GetRequiredService<ApplicationDbContext>();
                     if (context.Database.IsSqlServer()) {
@@ -55,6 +30,8 @@ namespace AuthApi {
                     logger.LogInformation("Host created.");
                 }
                 catch (Exception ex) {
+                    logger.LogError(ex, "An error occurred while migrating or seeding the database.");
+                    throw;
                 }
             }
             await host.RunAsync();
