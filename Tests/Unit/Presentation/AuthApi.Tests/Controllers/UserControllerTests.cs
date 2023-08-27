ba259263196f6321cdf98e86936c18c23d85f1e3
Or Hermoni <128356557+PixelPirate365@users.noreply.github.com>
2023-08-27 19:50:30 +0300

    usercontroller unit test error

---
+++ Tests/Unit/Presentation/AuthApi.Tests/Controllers/UserControllerTests.cs
@@ -0,0 +1,118 @@
+﻿using AuthApi.Controllers;
+using AuthService.Application.Handlers.User.Commands.ChangeUserPassword;
+using AuthService.Application.Handlers.User.Commands.CreateUser;
+using AuthService.Application.Handlers.User.Commands.DeleteUser;
+using AuthService.Application.Handlers.User.Commands.RefreshUserToken;
+using AuthService.Application.Handlers.User.Queries.AuthenticateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AuthApi.Tests.Controllers {
+    public class UserControllerTests {
+        private readonly UserController _controller;
+        private readonly Mock<IMediator> _mediator;
+        public UserControllerTests()
+        {
+            _mediator = new Mock<IMediator>();
+            _controller = new(_mediator.Object);
+        }
+        [Fact]
+        public async Task CreateUser_Successfully() {
+            //Arrange
+            var command = new CreateUserCommand();
+            //Act
+            var result = await _controller.CreateUser(command);
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+        [Fact]
+        public async Task ChangePassword_Successfully() {
+            //Arange
+            var command = new ChangeUserPasswordCommand();
+            //Act
+            var result = await _controller.ChangePassword(command);
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+        [Fact]
+        public async Task Login_Successfully() {
+            //Arange
+            var query = new AuthenticateUserQuery();
+            //Act
+            var result = await _controller.Login(query);
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+        [Fact]
+        public async Task Delete_Successfully() {
+            //Act
+            var result = await _controller.DeleteUser();
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+        [Fact]
+        public async Task RefreshToken_Successfully() {
+            //Arange
+            var command = new RefreshUserTokenCommand();
+            //Act
+            var result = await _controller.RefreshToken(command);
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+        }
+        [Fact]
+        public async Task CreateUser_Unsuccessfully() {
+            //Arrange
+            _mediator.Setup(x => x.Send(It.IsAny<CreateUserCommand>(), It.IsAny<CancellationToken>())).Throws(new Exception());
+            //Act
+            var result = async () => await _controller.CreateUser(new CreateUserCommand());
+            //Assert
+            var exception = await Assert.ThrowsAsync<Exception>(result);
+            Assert.NotNull(exception);
+        }
+        [Fact]
+        public async Task ChangePassword_Unsuccessfully() {
+            //Arrange
+            _mediator.Setup(x => x.Send(It.IsAny<ChangeUserPasswordCommand>(), It.IsAny<CancellationToken>())).Throws(new Exception());
+            //Act
+            var result = async () => await _controller.ChangePassword(new ChangeUserPasswordCommand());
+            //Assert
+            var exception = await Assert.ThrowsAsync<Exception>(result);
+            Assert.NotNull(exception);
+        }
+        [Fact]
+        public async Task Login_Unsuccessfully() {
+            //Arrange
+            _mediator.Setup(x => x.Send(It.IsAny<AuthenticateUserQuery>(), It.IsAny<CancellationToken>())).Throws(new Exception());
+            //Act
+            var result = async () => await _controller.Login(new AuthenticateUserQuery());
+            //Assert
+            var exception = await Assert.ThrowsAsync<Exception>(result);
+            Assert.NotNull(exception);
+        }
+        [Fact]
+        public async Task DeleteUser_Unsuccessfully() {
+            //Act
+            var result = async () => await _controller.DeleteUser();
+            //Assert
+            var exception = await Assert.ThrowsAsync<Exception>(result);
+            Assert.NotNull(exception);
+        }
+        [Fact]
+        public async Task RefreshToken_Unsuccessfully() {
+            //Arrange
+            _mediator.Setup(x => x.Send(It.IsAny<RefreshUserTokenCommand>(), It.IsAny<CancellationToken>())).Throws(new Exception());
+            //Act
+            var result = async () => await _controller.RefreshToken(new RefreshUserTokenCommand());
+            //Assert
+            var exception = await Assert.ThrowsAsync<Exception>(result);
+            Assert.NotNull(exception);
+        }
+    }
+}
