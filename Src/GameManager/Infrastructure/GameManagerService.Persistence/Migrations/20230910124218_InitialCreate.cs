ef3b0dead9bd1b8f9b5a77e14e83f87b9219a77b
Usman Tahir <Usman.tahir@synergy-it.com>
2023-09-10 17:42:52 +0500

    fix issues in api

---
+++ Src/GameManager/Infrastructure/GameManagerService.Persistence/Migrations/20230910124218_InitialCreate.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameManagerService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    AccountId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    PlayerColor = table.Column<int>(type: "int", nullable: false),
+                    TotalWin = table.Column<int>(type: "int", nullable: false),
+                    TotalLose = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FriendGameRequests",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlayerSenderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlayerRecieverId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FriendGameRequests", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FriendGameRequests_Players_PlayerRecieverId",
+                        column: x => x.PlayerRecieverId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FriendGameRequests_Players_PlayerSenderId",
+                        column: x => x.PlayerSenderId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BetAmount = table.Column<int>(type: "int", nullable: false),
+                    PlayerId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Games_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MatchMakings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlayerSenderId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    RandomMatchMakerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchMakings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MatchMakings_Players_PlayerSenderId",
+                        column: x => x.PlayerSenderId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MatchMakings_Players_RandomMatchMakerId",
+                        column: x => x.RandomMatchMakerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GamePlayers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GameId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlayerOneId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    PlayerTwoId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GamePlayers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GamePlayers_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GamePlayers_Players_PlayerOneId",
+                        column: x => x.PlayerOneId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GamePlayers_Players_PlayerTwoId",
+                        column: x => x.PlayerTwoId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GameStates",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GameId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CurrentPlayerId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    BoardState = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameStates", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GameStates_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_GameStates_Players_CurrentPlayerId",
+                        column: x => x.CurrentPlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Moves",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GamePlayersId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FromPoint = table.Column<int>(type: "int", nullable: false),
+                    ToPoint = table.Column<int>(type: "int", nullable: false),
+                    DiceRollOne = table.Column<int>(type: "int", nullable: false),
+                    DiceRollTwo = table.Column<int>(type: "int", nullable: false),
+                    GameId = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moves", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Moves_GamePlayers_GamePlayersId",
+                        column: x => x.GamePlayersId,
+                        principalTable: "GamePlayers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Moves_Games_GameId",
+                        column: x => x.GameId,
+                        principalTable: "Games",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FriendGameRequests_PlayerRecieverId",
+                table: "FriendGameRequests",
+                column: "PlayerRecieverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FriendGameRequests_PlayerSenderId",
+                table: "FriendGameRequests",
+                column: "PlayerSenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlayers_GameId",
+                table: "GamePlayers",
+                column: "GameId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlayers_PlayerOneId",
+                table: "GamePlayers",
+                column: "PlayerOneId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GamePlayers_PlayerTwoId",
+                table: "GamePlayers",
+                column: "PlayerTwoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Games_PlayerId",
+                table: "Games",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameStates_CurrentPlayerId",
+                table: "GameStates",
+                column: "CurrentPlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameStates_GameId",
+                table: "GameStates",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchMakings_PlayerSenderId",
+                table: "MatchMakings",
+                column: "PlayerSenderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchMakings_RandomMatchMakerId",
+                table: "MatchMakings",
+                column: "RandomMatchMakerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_GameId",
+                table: "Moves",
+                column: "GameId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Moves_GamePlayersId",
+                table: "Moves",
+                column: "GamePlayersId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FriendGameRequests");
+
+            migrationBuilder.DropTable(
+                name: "GameStates");
+
+            migrationBuilder.DropTable(
+                name: "MatchMakings");
+
+            migrationBuilder.DropTable(
+                name: "Moves");
+
+            migrationBuilder.DropTable(
+                name: "GamePlayers");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Players");
+        }
+    }
+}
