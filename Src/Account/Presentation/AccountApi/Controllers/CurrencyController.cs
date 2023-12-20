e83cddc214616bb3ce1d76750120b6e27bb60cfb
Or Hermoni <128356557+PixelPirate365@users.noreply.github.com>
2023-12-20 12:32:45 +0200

    finish currency integration

--- Src/Account/Presentation/AccountApi/Controllers/CurrencyController.cs
+++ Src/Account/Presentation/AccountApi/Controllers/CurrencyController.cs
@@ -5,26 +5,26 @@ using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AccountApi.Controllers {
-    //public class CurrencyController : BaseController<CurrencyController> {
-    //    private readonly IMediator _mediator;
-    //    public CurrencyController(IMediator mediator) {
-    //        _mediator = mediator;
-    //    }
-    //    [HttpPost(nameof(CollectDailyReward))]
-    //    public async Task<ActionResult<AccountTotalCoinsResponse>> CollectDailyReward() {
-    //        var response = await _mediator.Send(new CollectDailyLoginRewardCommand());
-    //        return Ok(response);
-    //    }
-    //    [HttpGet(nameof(GetTopCurrencyAccountProfiles))]
-    //    public async Task<ActionResult<List<GetTopCurrencyAccountProfileResponse>>> GetTopCurrencyAccountProfiles(int maximumNumberOfProfiles) {
-    //        var response = await _mediator.Send(
-    //            new GetTopCurrencyAccountProfilesQuery() { MaximumNumberOfProfiles = maximumNumberOfProfiles });
-    //        return Ok(response);
-    //    }
-    //    [HttpPost(nameof(UpdateAccountProfileCurrency))]
-    //    public async Task<ActionResult<UpdateAccountCurrencyResponse>> UpdateAccountProfileCurrency([FromBody] UpdateAccountCurrencyCommand command) {
-    //        var response = await _mediator.Send(command);
-    //        return Ok(response);
-    //    }
-    //}
+    public class CurrencyController : BaseController<CurrencyController> {
+        private readonly IMediator _mediator;
+        public CurrencyController(IMediator mediator) {
+            _mediator = mediator;
+        }
+        [HttpPost(nameof(CollectDailyReward))]
+        public async Task<ActionResult<AccountTotalCoinsResponse>> CollectDailyReward() {
+            var response = await _mediator.Send(new CollectDailyLoginRewardCommand());
+            return Ok(response);
+        }
+        [HttpGet(nameof(GetTopCurrencyAccountProfiles))]
+        public async Task<ActionResult<List<GetTopCurrencyAccountProfileResponse>>> GetTopCurrencyAccountProfiles(int maximumNumberOfProfiles) {
+            var response = await _mediator.Send(
+                new GetTopCurrencyAccountProfilesQuery() { MaximumNumberOfProfiles = maximumNumberOfProfiles });
+            return Ok(response);
+        }
+        [HttpPost(nameof(UpdateAccountProfileCurrency))]
+        public async Task<ActionResult<UpdateAccountCurrencyResponse>> UpdateAccountProfileCurrency([FromBody] UpdateAccountCurrencyCommand command) {
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+    }
 }
